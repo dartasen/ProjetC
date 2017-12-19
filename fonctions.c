3b9f9ca65feb232109a7e2e9e11a0c73332c0dc3
Dartasen <iCleeem@smocraft.fr>
2017-12-19 11:54:37 +0100

    Update fonctions.c

--- fonctions.c
+++ fonctions.c
@@ -1,18 +1,1 @@
 #include "Liste.h"
-
-
-
-
-Liste ajouterEnTete(Liste l,int x)
-{
- Maillon *m;
- m = (Maillon*)malloc(sizeof(Maillon));
- if(m == NULL)
- {
-   printf("Pb lors d'ajouterEnTete\n");
-   exit(1);
- }
- m->v = x;
- m->suiv = l;
- return m;
- }
