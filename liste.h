4b875d3de79db382a079ff578d6bdbd03f00499b
Dartasen <iCleeem@smocraft.fr>
2017-12-19 11:30:37 +0100

    Update liste.h

--- liste.h
+++ liste.h
@@ -1,4 +1,28 @@
-/* Fonction pour compter les mots clefs */
+MotClef MotVide(void) {
+	return NULL;
+}
+
+MotClef insererEnTete(MotClef m, char mot[]) {
+	MotClef sauv = m;
+	m = (MotClef) malloc(sizeof(maillonMot));
+
+	if (m == NULL) {
+		printf("Erreur de malloc de motClef \n");
+	    exit(1);
+	}
+
+	strcpy(m->mot, mot);
+	m->suiv = m;
+
+	return m;
+}
+
+MotClef inserer(MotClef m, char mot[]) {
+	if (m == MotVide())
+		return insererEnTete(m, mot);
+	
+	if (strcmp())
+}
 
 int compter(MotClef m) {
 	if (m == NULL)
@@ -6,4 +30,8 @@ int compter(MotClef m) {
 
 	return 1 + compter(m->suiv);
 }
-bite
+
+int main(void) {
+
+	return 0;
+}
