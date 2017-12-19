d20268f067203317693fd01b2884295f54af5a34
Dartasen <iCleeem@smocraft.fr>
2017-12-19 20:18:07 +0100

    Update prototype.h

--- lib/prototype.h
+++ lib/prototype.h
@@ -1,6 +1,14 @@
-//On insére ici les prototypes des fonctions
+//On insère ici les prototypes des fonctions
 
 void menu(void);
 
 Ouvrage saisirOuvrage(void);
 void afficherOuvrage(Ouvrage o);
+
+void afficherMotClef(MotClef m);
+int longueur(MotClef m);
+MotClef supprimer(MotClef m, char mot[]);
+MotClef supprimerEnTete(MotClef m);
+MotClef inserer(MotClef m, char mot[]);
+MotClef insererEnTete(MotClef m, char mot[]);
+MotClef MotVide(void);
