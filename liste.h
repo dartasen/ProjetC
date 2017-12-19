ceb66dc27a4350ca96b64fb85630772c50146e46
Dartasen <iCleeem@smocraft.fr>
2017-12-19 11:42:49 +0100

    Update liste.h

--- liste.h
+++ liste.h
@@ -20,18 +20,51 @@ MotClef insererEnTete(MotClef m, char mot[]) {
 MotClef inserer(MotClef m, char mot[]) {
 	if (m == MotVide())
 		return insererEnTete(m, mot);
-	
-	if (strcmp())
+
+	if (strcmp(m->mot, mot) < 0)
+		return insererEnTete(m, mot);
+
+	if (strcmp(m->mot, mot) == 0)
+		return m;
+
+	m->suiv = inserer(m->suiv, m);
+
+	return m;
 }
 
-int compter(MotClef m) {
+MotClef supprimerEnTete(MotClef m) {
+	maillonMot* temp;
+
+	if (m == NULL) {
+		printf("Erreur lors de la suppression en tête \n");
+		exit(1);
+	}
+
+	temp = m;
+	m = m->suiv;
+	free(temp);
+
+	return m;
+}
+
+MotClef supprimer(MotClef m, char mot[]) {
+	if (m == NULL)
+		return m;
+
+	if (strcmp(m->mot, mot) < 0)
+		return m;
+
+	if (strcmp(m->mot, mot) == 0)
+		return supprimerEnTete(m);
+
+	m->suiv = supprimer(m->suiv, mot);
+
+	return m;
+}
+
+int longueur(MotClef m) {
 	if (m == NULL)
 		return 0;
 
-	return 1 + compter(m->suiv);
-}
-
-int main(void) {
-
-	return 0;
+	return 1 + longueur(m->suiv);
 }
