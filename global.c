85b9004b3c70aad3d79242def3e3b1e600da81f8
Dartasen <iCleeem@smocraft.fr>
2017-12-19 15:25:45 +0100

    Update global.c

--- global.c
+++ global.c
@@ -1,7 +1,46 @@
 #include "liste.h"
 
-int main(void) {
+void menu(void) {
+ int choix = 0;
 
+ printf("**********************\n");
+ printf("*    BIBLIOTHEQUE    *\n");
+ printf("**********************\n\n");
+
+ while (choix != 8) {
+
+   printf("1) ???\n");
+   printf("2) ???\n");
+   printf("8) Quitter \n");
+
+   printf("Votre choix >");
+   scanf("%d", &choix);
+
+   //Si le choix est de quitter, on arrête
+   if (choix == 8)
+	   return;
+
+  //Si le choix est incorrect, on relance la boucle
+   if (choix < 1 || choix > 2) {
+      printf("Le choix est incorrect :( \n");
+      continue;
+   }
+ }
+
+ switch(choix) {
+
+ 	 case 1:
+
+	 break;
+
+ 	 case 2:
+
+	 break;
+ }
+}
+
+
+int main(void) {
  menu();
 
  return 0;
