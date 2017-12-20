82d530a63f967bfe1858509d53e51ef7eb1f5856
Afougue <34677113+Afougue@users.noreply.github.com>
2017-12-20 11:57:07 +0100

    Update lecteur.c

--- lecteur.c
+++ lecteur.c
@@ -59,3 +59,88 @@ int comparerNomPrenom(char* nom, char* prenom, char* nom1, char* prenom1) {
 
 	return -1;
 }
+#include "lib/lib.h"
+
+Lecteur saisirLecteur(void) {  
+  Lecteur l;
+
+  printf("Saisissez le numéro du lecteur :\n");
+  scanf("%d%*c", &l.numLecteur);
+
+  printf("Saisir le nom du lecteur \n");
+  fgets(l.nom, sizeof(l.nom), stdin);
+  l.nom[strlen(l.nom) - 1] = '\0';
+
+ printf("Saisir le prénom du lecteur \n");
+  fgets(l.prenom, sizeof(l.prenom), stdin);
+  l.prenom[strlen(l.prenom) - 1] = '\0';
+
+
+        
+  l.emprunt = NULL;
+
+return l;
+}
+
+void afficheLecteur(Lecteur l){
+
+	printf("\n///////////////////////////\n");
+
+	printf("Lecteur n° %d\n", l.numLecteur);
+	printf("Nom : %s\n", l.nom);
+	printf("Prénom : %s\n", l.prenom);
+
+	printf("///////////////////////////\n\n");
+}
+
+void DecalerADroite(Lecteur **l, int n, int i) {
+
+	int j;
+	for( j = n-1; j >= i; j--)
+	l[j+1] = l[j];
+}
+
+void DecalerAGauche(Lecteur **l, int n, int i) {
+	
+	int j;
+	for( j = i; j <= n-1; j++)
+	l[j] = l[j+1];
+}
+
+int ajouterLecteur(Lecteur **tab, Lecteur l, int nbelem, int tmax) {
+
+	int i;
+	if(nbelem == tmax)
+	{
+	printf("Erreur, tableau de Lecteur plein");
+	return -1; // Du coup faut le realloc pour augumenter la taille du tab
+	}
+
+	// i = rechercheDicho pour trouver la position d'insertion du Lecteur
+
+	DecalerADroite(tab,nbelem,i);
+	tab[i] = l;
+	return nbelem +1 ;
+}
+
+int supprimerLecteur(Lecteur **tab, int i, int nbelem) {
+	
+	if(nbelem == 0)
+	{
+	printf("Tableau deja vide");
+	return -1;
+	}
+	
+	DecalerAGauche(tab,nbelem,i);
+	
+	return nbelem -1 ;
+}
+
+	
+
+
+
+	 
+
+
+
