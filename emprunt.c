aa4895173fc26f3a9607744f63defd9474a6e3b1
Dartasen <iCleeem@smocraft.fr>
2017-12-20 11:41:50 +0100

    Update emprunt.c

--- emprunt.c
+++ emprunt.c
@@ -12,9 +12,3 @@ ListeEmprunt nouvelEmprunt(Lecteur l, Ouvrage o)
 		}
 	l.emprunt=insererEnTeteEmprunt(l.emprunt,o);
 }
-
-void sauvegardeBin (Ouvrage **tabOuvrage , Lecteur **tabLecteur, int nbOuvrage , int nbLecteur)
-{
-	fopen("sauv.bin","wb");
-	f.write()
-}
