0adc5c54c997dfafc80b7a011ea75b41fa93b7f6
Dartasen <iCleeem@smocraft.fr>
2017-12-20 11:05:31 +0100

    Update prototype.h

--- lib/prototype.h
+++ lib/prototype.h
@@ -18,3 +18,4 @@ Emprunt nouvelEmprunt(Lecteur l, Ouvrage o);
 void trierEchange(Lecteur* tab[], int nb);
 int rechercheEchange(Lecteur* tab[], int nb);
 void echangerEchange(Lecteur* tab[], int i, int j);
+int comparerNomPrenom(char* nom, char* prenom, char* nom1, char* prenom);
