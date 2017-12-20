7fa1fe994342af578e2b41da0caf76f42172341d
Dartasen <iCleeem@smocraft.fr>
2017-12-20 11:29:46 +0100

    Update global.c

--- global.c
+++ global.c
@@ -43,25 +43,6 @@ void menu(void) {
   }
 }
 
-ListeEmprunt nouvelEmprunt(Lecteur l, Ouvrage o)
-{
-	if (o.quantite - o.quantiteEmprunt <=0)
-		{
-			printf("Impossible d'emprunter l'ouvrage \n");
-			return l.emprunt;
-		}
-	if (longueurListeEmprunt(l.emprunt) ==5)
-		{
-			printf("Vous avez déjà emprunter 5 livres \n");
-			return l.emprunt;
-		}
-	l.emprunt=insererEnTeteEmprunt(l.emprunt,o);
-}
-void sauvegardeBin (Ouvrage **tabOuvrage , Lecteur **tabLecteur, int nbOuvrage , int nbLecteur)
-{
-	fopen("sauv.bin","wb");
-	f.write()
-}
 int main(void) {
  menu();
 
