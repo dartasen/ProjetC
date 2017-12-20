d38f50f70c7b0cc1d9246ae73f49877d82d94758
Dartasen <iCleeem@smocraft.fr>
2017-12-20 11:37:46 +0100

    Update Protomotclef.h

--- lib/Protomotclef.h
+++ lib/Protomotclef.h
@@ -1,7 +1,1 @@
-void afficherMotClef(MotClef m);
-int longueurMotClef(MotClef m);
-MotClef supprimerMotClef(MotClef m, char mot[]);
-MotClef supprimerEnTeteMotClef(MotClef m);
-MotClef insererMotClef(MotClef m, char mot[]);
-MotClef insererEnTeteMotClef(MotClef m, char mot[]);
-MotClef MotVideMotClef(void);
+
