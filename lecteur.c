0b924be20132911c2957156d6be1f4b618d8a907
Dartasen <iCleeem@smocraft.fr>
2017-12-20 11:55:46 +0100

    Update lecteur.c

--- lecteur.c
+++ lecteur.c
@@ -10,19 +10,16 @@ Lecteur saisirLecteur(void) {
   fgets(l.nom, sizeof(l.nom), stdin);
   l.nom[strlen(l.nom) - 1] = '\0';
 
- printf("Saisir le prénom du lecteur \n");
+  printf("Saisir le prénom du lecteur \n");
   fgets(l.prenom, sizeof(l.prenom), stdin);
   l.prenom[strlen(l.prenom) - 1] = '\0';
-
-
-        
+	
   l.emprunt = NULL;
 
 return l;
 }
 
-void afficheLecteur(Lecteur l){
-
+void afficheLecteur(Lecteur l) {
 	printf("\n///////////////////////////\n");
 
 	printf("Lecteur n° %d\n", l.numLecteur);
@@ -30,4 +27,35 @@ void afficheLecteur(Lecteur l){
 	printf("Prénom : %s\n", l.prenom);
 
 	printf("///////////////////////////\n\n");
+}
+
+void trierEchange(Lecteur* tab[], int nb) {
+	int pge;
+
+	while (nb > 1) {
+		pge = rechercheEchange(tab, nb);
+		echangerEchange(tab, pge, nb -1);
+		nb --;
+	}
+}
+
+int rechercheEchange(Lecteur* tab[], int nb) {
+	int pge = 0, i;
+
+	for (i=1; i < nb; i++)
+
+		if (comparerNomPrenom(tab[i]->nom, tab[i]->prenom, tab[pge]->nom, tab[pge]->prenom) > 0)
+			pge = i;
+
+	return pge;
+}
+
+int comparerNomPrenom(char* nom, char* prenom, char* nom1, char* prenom1) {
+
+	if (strcmp(nom, nom1) > 0) {
+		
+	}
+
+
+	return -1;
 }
