ff6f70dd02256b0c9e0577d607bd61dd120f4f97
Afougue <34677113+Afougue@users.noreply.github.com>
2017-12-20 10:35:30 +0100

    Update fonctions.c

--- fonctions.c
+++ fonctions.c
@@ -1,1 +1,14 @@
-
+Lecteur saisirLecteur(void) {
+  
+  Lecteur l;
+  printf("Saisissez le nom du lecteur");
+  scanf("%d", &l.nom);
+  
+  printf("Saisissez le prénom du lecteur");
+  scanf("%d*c", &l.prenom);
+        
+  printf("Saisissez le numéro du lecteur");
+  scanf("%s", l.numLecteur);
+        
+  l.Emprunt = NULL;
+}
