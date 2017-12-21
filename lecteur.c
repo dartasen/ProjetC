ae715dd60a85a8dbb2018ac72eb9686a307e234c
Dartasen <iCleeem@smocraft.fr>
2017-12-21 18:08:05 +0100

    Update lecteur.c

--- lecteur.c
+++ lecteur.c
@@ -1,6 +1,7 @@
 #include "lib/lib.h"
 
-Lecteur saisirLecteur(void) {  
+Lecteur saisirLecteur(void) {
+  Emprunt m = EmpruntVide();
   Lecteur l;
 
   printf("Saisissez le numéro du lecteur :\n");
@@ -14,19 +15,18 @@ Lecteur saisirLecteur(void) {
   fgets(l.prenom, sizeof(l.prenom), stdin);
   l.prenom[strlen(l.prenom) - 1] = '\0';
 	
-  l.emprunt = NULL;
+  l.emprunt = m;
 
 return l;
 }
 
 void afficheLecteur(Lecteur l) {
-	printf("\n///////////////////////////\n");
+	printf("> Lecteur n° %d\n", l.numLecteur);
+	printf("> Nom : %s\n", l.nom);
+	printf("> Prénom : %s\n", l.prenom);
+	afficherEmprunt(l.emprunt);
 
-	printf("Lecteur n° %d\n", l.numLecteur);
-	printf("Nom : %s\n", l.nom);
-	printf("Prénom : %s\n", l.prenom);
-
-	printf("///////////////////////////\n\n");
+	printf("\n");
 }
 
 void trierEchange(Lecteur* tab[], int nb) {
@@ -68,72 +68,16 @@ int comparerNomPrenom(char* nom, char* prenom, char* nom1, char* prenom1) {
 	return comp;
 }
 
-Lecteur saisirLecteur(void) {  
-  Lecteur l;
-
-  printf("Saisissez le numéro du lecteur :\n");
-  scanf("%d%*c", &l.numLecteur);
-
-  printf("Saisir le nom du lecteur \n");
-  fgets(l.nom, sizeof(l.nom), stdin);
-  l.nom[strlen(l.nom) - 1] = '\0';
-
-  printf("Saisir le prénom du lecteur \n");
-  fgets(l.prenom, sizeof(l.prenom), stdin);
-  l.prenom[strlen(l.prenom) - 1] = '\0'
-	  
-  l.emprunt = NULL;
-
-return l;
-}
-
-void afficheLecteur(Lecteur l){
-
-	printf("\n///////////////////////////\n");
-
-	printf("Lecteur n° %d\n", l.numLecteur);
-	printf("Nom : %s\n", l.nom);
-	printf("Prénom : %s\n", l.prenom);
-
-	printf("///////////////////////////\n\n");
-}
-
-void DecalerADroite(Lecteur **l, int n, int i) {
+void DecalerADroite(Lecteur* tab[], int n, int i) {
 	int j;
 	
 	for (j = n-1; j >= i; j--)
-		l[j+1] = l[j];
+		tab[j+1] = tab[j];
 }
 
-void DecalerAGauche(Lecteur **l, int n, int i) {
+void DecalerAGauche(Lecteur* tab[], int n, int i) {
 	int j;
 	
-	for( j = i; j <= n-1; j++)
-		l[j] = l[j+1];
-}
-
-int ajouterLecteur(Lecteur **tab, Lecteur l, int nbelem, int tmax) {
-	int i;
-	
-	if(nbelem == tmax) {
-		printf("Erreur, tableau de Lecteur plein");
-		return -1;
-	}
-
-	DecalerADroite(tab,nbelem,i);
-	tab[i] = l;
-	
-	return nbelem +1 ;
-}
-
-int supprimerLecteur(Lecteur **tab, int i, int nbelem) {
-	
-	if(nbelem == 0) {
-		printf("Tableau deja vide");
-		return -1;
-	}
-	
-	DecalerAGauche(tab,nbelem,i);
-	
-	return nbelem -1 ;
+	for (j = i; j <= n-1; j++)
+		tab[j] = tab[j+1];
 }
