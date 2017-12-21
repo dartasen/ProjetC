782c156b3b8b9467e23f6a3b5e77440cc7ac74cc
Dartasen <iCleeem@smocraft.fr>
2017-12-21 18:10:03 +0100

    Update lecteur.h

--- lib/lecteur.h
+++ lib/lecteur.h
@@ -1,2 +1,8 @@
 Lecteur saisirLecteur(void);
 void afficheLecteur(Lecteur l);
+void trierEchange(Lecteur* tab[], int nb);
+void echangerEchange(Lecteur* tab[], int i, int j);
+int rechercheEchange(Lecteur* tab[], int nb);
+int comparerNomPrenom(char* nom, char* prenom, char* nom1, char* prenom1);
+void DecalerADroite(Lecteur* tab[], int n, int i);
+void DecalerAGauche(Lecteur* tab[], int n, int i);
