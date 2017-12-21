e7d6dc0262c6be19ed89476ccf22adf4728d263d
Dartasen <iCleeem@smocraft.fr>
2017-12-21 18:10:23 +0100

    Update emprunt.h

--- lib/emprunt.h
+++ lib/emprunt.h
@@ -1,1 +1,8 @@
-ListeEmprunt nouvelEmprunt(Lecteur l, Ouvrage o);
+void nouvelEmprunt(Lecteur l, Ouvrage o);
+Emprunt EmpruntVide(void);
+Emprunt insererEnTeteEmprunt(Emprunt m, char cote[]);
+Emprunt insererEmprunt(Emprunt m, char cote[]);
+Emprunt supprimerEnTeteEmprunt(Emprunt m);
+Emprunt supprimerEmprunt(Emprunt m, char cote[]);
+int longueurEmprunt(Emprunt m);
+void afficherEmprunt(Emprunt m);
