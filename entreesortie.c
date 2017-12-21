15f900822d881c4a42c57a99d8f7be0a15421f01
Dartasen <iCleeem@smocraft.fr>
2017-12-21 18:08:26 +0100

    Update entreesortie.c

--- entreesortie.c
+++ entreesortie.c
@@ -1,5 +1,4 @@
-void sauvegardeBin (Ouvrage **tabOuvrage , Lecteur **tabLecteur, int nbOuvrage , int nbLecteur)
-{
-	fopen("sauv.bin","wb");
-	f.write()
+#include "lib/lib.h"
+
+void sauvegardeBin (Ouvrage **tabOuvrage , Lecteur **tabLecteur, int nbOuvrage , int nbLecteur) {
 }
