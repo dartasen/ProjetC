15571b339e788c2b56876487aa4d76e2c1c3f3e9
Dartasen <iCleeem@smocraft.fr>
2017-12-26 17:34:25 +0100

    Update entreesortie.c

--- entreesortie.c
+++ entreesortie.c
@@ -1,4 +1,55 @@
 #include "lib/lib.h"
 
-void sauvegardeBin (Ouvrage **tabOuvrage , Lecteur **tabLecteur, int nbOuvrage , int nbLecteur) {
+void sauvegarde(Ouvrage* tabO[], Lecteur* tabL[], int nbO, int nbL) {
+	int etat = 0;
+
+	etat += sauvegardeOuvrage(tabO, nbO);
+	etat += sauvegardeLecteur(tabL, nbL);
+
+	if (etat < 0)
+		exit(1);
+
+	printf("\nSAUVEGARDE EFFECTUEE !\n");
+}
+
+
+int sauvegardeOuvrage(Ouvrage* tab[], int nbr) {
+	FILE* fichier = fopen("ouvrage.don", "w");
+	Ouvrage m;
+	int i;
+
+	if (fichier == NULL) {
+		printf("Erreur lors de l'ouverture du fichier ouvrage.don \n");
+		fclose(fichier);
+
+		return -1;
+	}
+
+	fprintf(fichier, "%d\n", nbr);
+
+	for (i = 0; i < nbr; i++) {
+		m = *tab[i];
+
+		fprintf(fichier, "%s\t%s\t%s\t%d\t%d\t", m.cote, m.titre, m.categorie, m.quantite, m.quantiteEmprunt);
+		ecrireMotClef(fichier, m.motclefs);
+	}
+
+	fclose(fichier);
+
+	return 1;
+}
+
+int sauvegardeLecteur(Lecteur* tab[], int nbr) {
+	FILE* fichier = fopen("lecteur.don", "w");
+
+	if (fichier == NULL) {
+		printf("Erreur lors de l'ouverture du fichier lecteur.don \n");
+		fclose(fichier);
+
+		return -1;
+	}
+
+	fclose(fichier);
+
+	return 1;
 }
