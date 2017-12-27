96c578edcf9a756aa9d328bf75016f250da54116
Criquix <moromain@live.fr>
2017-12-27 10:59:49 +0100

    Update entreesortie.h
    ajout des fonctions de sauvegarde Binaires

--- lib/entreesortie.h
+++ lib/entreesortie.h
@@ -2,3 +2,6 @@ void menu(Ouvrage* tabO[], Lecteur* tabL[], int* nbO, int* nbL);
 void sauvegarde(Ouvrage* tabO[], Lecteur* tabL[], int nbO, int nbL);
 int sauvegardeLecteur(Lecteur* tab[], int nbr);
 int sauvegardeOuvrage(Ouvrage* tab[], int nbr);
+void sauvegardeBin(Ouvrage **tabO, Lecteur ** tabL, int nbO, int nbL);
+int sauvegardeBinMotClef(Ouvrage ouv);
+int sauvegardeBinEmprunt(Lecteur l);
