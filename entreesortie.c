fd51be3b49c08f923ce8dca6ee9ac4af2edf6d64
Dartasen <iCleeem@smocraft.fr>
2017-12-28 21:23:06 +0100

    Update entreesortie.c

--- entreesortie.c
+++ entreesortie.c
@@ -1,5 +1,77 @@
 #include "lib/lib.h"
 
+Ouvrage** chargeFouvrage(FILE* fichier, int* nbO) {
+	Ouvrage **tab, **newtab, o;
+	int i = 0;
+
+	fscanf(fichier, "%d", nbO);
+
+	tab = (Ouvrage**) malloc(*nbO * sizeof(Ouvrage*));
+	tab[0] = (Ouvrage*) malloc(sizeof(Ouvrage));
+
+	if (tab == NULL || tab[0] == NULL) {
+		printf("Erreur de malloc du tableau ouvrage \n");
+		free(tab);
+		exit(1);
+	}
+
+	o = lireOuvrage(fichier);
+
+	while (!feof(fichier)) {
+		if (i == *nbO) {
+			newtab = (Ouvrage**) realloc(tab, (*nbO + 1) * sizeof(Ouvrage*));
+
+			if (newtab != NULL) {
+				tab = newtab;
+				free(newtab);
+			} else {
+				printf("Erreur de ralloc du tab ouvrage \n");
+				free(newtab);
+				exit(1);
+			}
+		}
+
+		tab[i] = (Ouvrage*) malloc(sizeof(Ouvrage));
+
+		if (tab[i] == NULL) {
+			printf("Erreur de malloc d'un ouvrage \n");
+			exit(1);
+		}
+
+		*tab[i] = o;
+		*nbO += 1;
+		i++;
+
+		o = lireOuvrage(fichier);
+	}
+
+	return tab;
+}
+
+Ouvrage** ajouterOuvrage(Ouvrage** tab, int* nbO, Ouvrage o) {
+	Ouvrage **aux;
+
+	aux = (Ouvrage**) realloc(tab, (*nbO + 1) * sizeof(Ouvrage*));
+
+	if (aux == NULL) {
+		printf("Erreur de ralloc du tab ouvrage \n");
+		free(aux);
+		exit(1);
+	}
+
+	aux[*nbO] = (Ouvrage*) malloc(sizeof(Ouvrage));
+
+	if (aux[*nbO] == NULL) {
+		printf("Erreur de malloc d'un ouvrage \n");
+		exit(1);
+	}
+
+	*aux[*nbO] = o;
+	*nbO += 1;
+
+	return aux;
+}
+
 void sauvegarde(Ouvrage* tabO[], Lecteur* tabL[], int nbO, int nbL) {
 	int etat = 0;
 
