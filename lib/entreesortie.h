fa73405030925d70b50945826945b4facb45c6cd
Dartasen <iCleeem@smocraft.fr>
2018-01-03 16:50:36 +0100

    Update entreesortie.h

--- lib/entreesortie.h
+++ lib/entreesortie.h
@@ -1,7 +1,7 @@
 void menu(Ouvrage* tabO[], Lecteur* tabL[], int* nbO, int* nbL);
+Ouvrage** SousMenuOuvrage(Ouvrage* tabO[], int* nbO);
 void sauvegarde(Ouvrage* tabO[], Lecteur* tabL[], int nbO, int nbL);
 int sauvegardeLecteur(Lecteur* tab[], int nbr);
 int sauvegardeOuvrage(Ouvrage* tab[], int nbr);
 Ouvrage** chargeFouvrage(FILE* fichier, int* nbO);
-Ouvrage** ajouterOuvrage(Ouvrage** tab, int* nbO, Ouvrage o);
 void streplace(char mot[], char remplacer, char remplacant);
