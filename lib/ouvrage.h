84615176b404a893dc4cce4c4ab7e548d59b3f75
Dartasen <iCleeem@smocraft.fr>
2018-01-03 16:50:50 +0100

    Update ouvrage.h

--- lib/ouvrage.h
+++ lib/ouvrage.h
@@ -1,3 +1,6 @@
 Ouvrage saisirOuvrage(void);
 void afficherOuvrage(Ouvrage o);
 Ouvrage lireOuvrage(FILE* fichier);
+int rechercherOuvrage(char cote[], Ouvrage* tabO[], int nbO);
+Ouvrage** ajouterOuvrage(Ouvrage** tab, int* nbO, Ouvrage o);
+Ouvrage** supprimerOuvrage(Ouvrage* tabO[], int* nbO, int i);
