afdfb6443b4bd5357e4c74c663caf045023093e8
Criquix <moromain@live.fr>
2018-01-07 23:31:55 +0100

    Update emprunt.h
    ajout de insérerEnTeteEmpruntBin

--- lib/emprunt.h
+++ lib/emprunt.h
@@ -2,6 +2,7 @@ void nouvelEmprunt(Lecteur l, Ouvrage o);
 Emprunt EmpruntVide(void);
 Emprunt insererEnTeteEmprunt(Emprunt m, char cote[]);
 Emprunt insererEmprunt(Emprunt m, char cote[]);
+Emprunt insererEnTeteEmpruntBin(Emprunt m, char cote[], Date date);
 Emprunt supprimerEnTeteEmprunt(Emprunt m);
 Emprunt supprimerEmprunt(Emprunt m, char cote[]);
 int longueurEmprunt(Emprunt m);
