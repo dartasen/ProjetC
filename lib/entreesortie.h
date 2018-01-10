343658ea8a0c53cbb14711f6a2675418bc5d009e
Dartasen <iCleeem@smocraft.fr>
2018-01-10 16:18:03 +0100

    Update entreesortie.h

--- lib/entreesortie.h
+++ lib/entreesortie.h
@@ -5,9 +5,7 @@ int sauvegardeLecteur(Lecteur* tab[], int nbr);
 int sauvegardeOuvrage(Ouvrage* tab[], int nbr);
 Ouvrage** chargeFouvrage(FILE* fichier, int* nbO);
 void streplace(char mot[], char remplacer, char remplacant);
-//sauvegardeBinaire
-void sauvegardeBinLecteur(Lecteur **tab, int nbLecteur,FILE *flot);
+void sauvegardeBinLecteur(Lecteur **tab, int nbLecteur);
 void sauvegardeBinEmprunt(Emprunt le, int nbEmprunt,FILE *flot);
 Lecteur ** chargementBinLecteur(FILE *flot);
 Emprunt chargerBinEmprunt(FILE *flot, int nbElem);
-
